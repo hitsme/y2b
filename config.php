65ef5c443f2785be3abef641cdbe5cdc0c8a1ffa
hitsme <1182681022@qq.com>
2018-10-17 12:42:13 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyB5zY2fq6YRSMxF7HwGb8rEQ8VVRxiO6ps');
-define('GJ_CODE', 'HK');
-define('SITE_NAME', 'you2beclub');
-define('TITLENAME', 'you2beclub');
+define('APIKEY', 'AIzaSyB5xlrKGEQa3r8nzwy_32CwprKpO7ftS7A');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'Hitsme');
+define('TITLENAME', 'Hitsme');
 define('EN2DEKEY', '32123wwerwertgfdgds');
 define('EMAIL', '1334347212@qq.com');
 define('NAME', 'admin');
@@ -18,12 +18,12 @@ define('LOGINHTML', '<!DOCTYPE html>
 <body>
 <center>
     <p id="login">
-    <h1>µÇÂ½</h1>
+    <h1>ÂµÃ‡Ã‚Â½</h1>
     <form method="post" style="margin:0px auto;">
-        <input type="text" required="required" placeholder="ÓÃ»§Ãû" name="name"></input>
+        <input type="text" required="required" placeholder="Ã“ÃƒÂ»Â§ÃƒÃ»" name="name"></input>
         <br>
-        <input type="password" required="required" placeholder="ÃÜÂë" name="password"></input>
-        <button class="but" type="submit">µÇÂ¼</button>
+        <input type="password" required="required" placeholder="ÃƒÃœÃ‚Ã«" name="password"></input>
+        <button class="but" type="submit">ÂµÃ‡Ã‚Â¼</button>
     </form>
     </p>
 </center>
