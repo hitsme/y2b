2c402bc346dde26bd16b205cb4ba542646c6dfa2
zxkke <869431096@qq.com>
2019-10-02 00:46:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyAbF_KymuScPEZ61g6fCWF6nqXBv3JdHVQ');
 define('GJ_CODE', 'US');
-define('SITE_NAME', 'y2b');
-define('TITLENAME', 'y2b');
-define('EN2DEKEY', '32123wwerwer323tgfdgds');
+define('SITE_NAME', 'sy2b');
+define('TITLENAME', 'sy2b');
+define('EN2DEKEY', '32123wwer12wer323tgfdgds');
 define('EMAIL', '1334347212@qq.com');
 define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('PASSWORD', 'xiaoke');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
